beb495f284d27b2ab7296c5c219930cb2a0ef53b
Swathika Ramathilagar <113713379+Swathika04@users.noreply.github.com>
2023-12-23 14:52:40 +0530

    Update Shortestpath1.java
    task 2

--- Shortestpath.java
+++ Shortestpath.java
@@ -1,20 +1,31 @@
 import java.util.Scanner;
 public class Shortestpath {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("enter the dimensions: ");
-        int dimrow=sc.nextInt();
-        int dimcol=sc.nextInt();
+        int dimrow = sc.nextInt();
+        int dimcol = sc.nextInt();
         System.out.println("enter the adventurer's position: ");
-        int advenrow= sc.nextInt();
-        int advencol= sc.nextInt();
+        int advenrow = sc.nextInt();
+        int advencol = sc.nextInt();
+        System.out.println("enter the monster's position: ");
+        int monsterrow = sc.nextInt();
+        int monstercol = sc.nextInt();
         System.out.println("enter the gold position: ");
-        int goldrow= sc.nextInt();
-        int goldcol= sc.nextInt();
-        int steps= minSteps(advenrow,advencol,goldrow,goldcol);
-        System.out.println("minimum num of steps to reach gold: "+steps);
+        int goldrow = sc.nextInt();
+        int goldcol = sc.nextInt();
+        int steps = minSteps(advenrow, advencol, goldrow, goldcol, monsterrow, monstercol);
+        if (steps == -1) {
+            System.out.println("No possible solution");
+        } else {
+            System.out.println("Minimum number of steps to reach gold: " + steps);
+        }
     }
-    private static int minSteps(int startrow,int startcol,int endrow,int endcol){
-        return Math.abs(startrow-endrow)+Math.abs(startcol-endcol);
+    private static int minSteps(int startrow, int startcol, int endrow, int endcol, int monsterrow, int monstercol) {
+        if ((startrow <= monsterrow && monsterrow <= endrow || endrow <= monsterrow && monsterrow <= startrow) &&
+                (startcol <= monstercol && monstercol <= endcol || endcol <= monstercol && monstercol <= startcol)) {
+            return -1;
+        }
+        return Math.abs(startrow - endrow) + Math.abs(startcol - endcol);
     }
 }
